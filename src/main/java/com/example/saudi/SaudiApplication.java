28d142877a9370445ed4903f2d241a46b17a68d7
wonandonly <wann416@sookmyung.ac.kr>
2025-04-09 16:35:20 +0900

    [chore] 프로젝트 생성 및 .gitignore 파일 작성

---
+++ src/main/java/com/example/saudi/SaudiApplication.java
@@ -0,0 +1,13 @@
+package com.example.saudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaudiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saudi/SaudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
